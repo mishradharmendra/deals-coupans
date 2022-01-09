298d322357f4556fbfe168437a78492e59071c2f
Dharmendra Mishra <mishra.dharmendra1987@gmail.com>
2022-01-09 20:22:01 +0530

    Update WalletServiceClient.java

--- coupon-service/src/main/java/com/cg/dealscoupons/couponservice/client/WalletServiceClient.java
+++ coupon-service/src/main/java/com/cg/dealscoupons/couponservice/client/WalletServiceClient.java
@@ -7,13 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "CASHBACK-SERVICE", url = "http://localhost:8080/api/wallet", configuration = MyRestTemplateConfig.class)
 public interface WalletServiceClient {
     @PostMapping(value = "/createWallet", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<Wallet> addWallet(@RequestBody Wallet wallet);
+    ResponseEntity<Wallet> addWallet(@RequestHeader("Authorization") String token,
+                                     @RequestBody Wallet wallet);
     @GetMapping(value = "/customer/{customerId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<Wallet> getByCustomerId(@PathVariable("customerId") String customerId);
+    ResponseEntity<Wallet> getByCustomerId(@RequestHeader("Authorization") String token,
+                                           @PathVariable("customerId") String customerId);
     @PostMapping(value = "/payMoney", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<Void> payMoney(@RequestBody WalletRequest wallet);
+    ResponseEntity<Void> payMoney(@RequestHeader("Authorization") String token,
+                                  @RequestBody WalletRequest wallet);
 }
